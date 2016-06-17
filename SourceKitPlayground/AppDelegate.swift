580a0b97693002a23a58896f04756a9f330f7b13
Ankit Aggarwal <ankit.spd@gmail.com>
2016-06-17 12:19:48 +0530

    Initial Commit

---
+++ SourceKitPlayground/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SourceKitPlayground
+//
+//  Created by Ankit Aggarwal on 17/06/16.
+//  Copyright © 2016 ankit.im. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SourceKitPlayground/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SourceKitPlayground
+//
+//  Created by Ankit Aggarwal on 17/06/16.
+//  Copyright © 2016 ankit.im. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
